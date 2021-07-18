53da8ec8e67afce20bf6d25f260a7788b7e00b4d
Chakrit Wichian <service@chakrit.net>
2021-07-18 19:56:49 +0700

    Create discord channel and permanent invite
    So we can invite ourselves in

--- discord.tf
+++ discord.tf
@@ -3,3 +3,18 @@ resource "discord_server" "openvac" {
   owner_id = "85950745328771072"
   region   = "singapore"
 }
+
+resource "discord_text_channel" "welcome" {
+  name      = "welcome"
+  server_id = discord_server.openvac.id
+}
+
+resource "discord_invite" "public-invite" {
+  channel_id = discord_text_channel.welcome.id
+  max_age    = 0
+}
+
+output "discord_invite_code" {
+  value     = discord_invite.public-invite.code
+  sensitive = false
+}
