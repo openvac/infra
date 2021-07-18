4bfdf480767ed9b138e705ff1a92154ad8ada765
Chakrit Wichian <service@chakrit.net>
2021-07-18 12:33:12 +0700

    Drive repository from list

--- main.tf
+++ main.tf
@@ -24,9 +24,30 @@ provider "github" {
   owner = "openvac"
 }
 
+locals {
+  repositories = {
+    "infra"   = "IaC for OpenVac",
+    "openvac" = "Main openvac codebase",
+  }
+}
 
-resource "github_repository" "infra" {
-  name        = "infra"
-  description = "IaC for OpenVac"
-  visibility  = "public"
+resource "github_repository" "public-repos" {
+  for_each = local.repositories
+
+  name           = each.key
+  description    = each.value
+  visibility     = "public"
+  is_template    = false
+  auto_init      = false
+  default_branch = "main"
+
+  has_issues    = true
+  has_projects  = false
+  has_wiki      = false
+  has_downloads = false
+
+  allow_merge_commit     = false
+  allow_squash_merge     = false
+  allow_rebase_merge     = true
+  delete_branch_on_merge = true
 }
